f78cb925294212ff6c3042b02d90ce058eb6a704
tisa17 <44108802+tisa17@users.noreply.github.com>
2022-06-30 03:48:20 -0500

    Create adventureworksQuery1.sql
    Olatunde Assignment 1 Complete

---
+++ Day 1 assignment/adventureworksQuery1.sql
@@ -0,0 +1,52 @@
+Select ProductID,Name,Color,ListPrice
+From Production.Product
+
+Select ProductID,Name,Color,ListPrice
+From Production.Product
+where Not ListPrice=0;
+
+Select ProductID,Name,Color,ListPrice
+From Production.Product
+Where Color IS NULL;
+
+Select ProductID,Name,Color,ListPrice
+From Production.Product
+Where Color IS NOT NULL;
+
+Select ProductID,Name,Color,ListPrice
+From Production.Product
+Where Color IS NOT NULL AND ListPrice>0;
+
+Select CONCAT(Name,Color)[Name And Color]
+From Production.Product
+where Color is not NULL
+
+Select CONCAT('Name: ',Name,'  --  COLOR: ',Color)[Name And Color]
+From Production.Product
+where Name  Like '%Crankarm' OR  Name Like 'chainring%' 
+order by ProductID asc
+
+Select ProductID, Name
+from Production.Product
+where ProductID between '400' and '500'
+
+Select ProductID,Name,Color
+from Production.Product
+where Color IN ('black','blue')
+
+Select*
+From Production.Product
+where Name Like 's%'Select TOP 6 Name, ListPriceFrom Production.Productwhere Name Like 's%'order by Nameselect TOP 5 Name, ListPrice
+from Production.Product
+where Name like '[AS]%'
+order by Name
+
+SELECT *
+FROM Production.Product
+WHERE Name LIKE 'SPO[^K]%'
+ORDER BY Name
+
+
+SELECT Distinct Color 
+FROM Production.Product
+ORDER BY Color desc
