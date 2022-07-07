f0cd1f0e685d895edc26de3537037e5c4dedcc59
tisa17 <44108802+tisa17@users.noreply.github.com>
2022-07-07 04:13:22 -0500

    update

--- Day 3 assignment/day3Query1.sql
+++ Day 3 assignment/day3Query1.sql
@@ -1,93 +1,111 @@
 --1
 SELECT DISTINCT CITY
-FROM Customers
-WHERE City IN (SELECT City FROM Employees)
+FROM CUSTOMERS
+WHERE CITY IN (SELECT CITY FROM EMPLOYEES)
 
---2a
+--2A
 
 SELECT DISTINCT CITY
 FROM CUSTOMERS 
 WHERE CITY NOT IN (SELECT CITY FROM EMPLOYEES)
 
 --2B
-select distinct c.city 
-from Customers c, Employees e 
-where c.City <> e.city;
+SELECT DISTINCT C.CITY 
+FROM CUSTOMERS C, EMPLOYEES E 
+WHERE C.CITY <> E.CITY;
 
 --3
-select p.productname , sum(od.quantity) [total order Quantities]
-from Products p inner join [Order Details] od
-on od.ProductID = p.ProductID 
-group by p.productname
+SELECT P.PRODUCTNAME , SUM(OD.QUANTITY) [TOTAL ORDER QUANTITIES]
+FROM PRODUCTS P INNER JOIN [ORDER DETAILS] OD
+ON OD.PRODUCTID = P.PRODUCTID 
+GROUP BY P.PRODUCTNAME
 
 --4
-select  b.city , sum(p.ProductID) [total products]
-from Products p inner join dbo.[Order Details] od
-on od.ProductID = p.ProductID inner join Orders a 
-on a.OrderID = od.OrderID inner join Customers b
-on b.CustomerID = a.CustomerID
-group by b.City;
+SELECT  B.CITY , SUM(P.PRODUCTID) [TOTAL PRODUCTS]
+FROM PRODUCTS P INNER JOIN [ORDER DETAILS] OD
+ON OD.PRODUCTID = P.PRODUCTID INNER JOIN ORDERS A 
+ON A.ORDERID = OD.ORDERID INNER JOIN CUSTOMERS B
+ON B.CUSTOMERID = A.CUSTOMERID
+GROUP BY B.CITY;
 
 
---5a
-select city 
-from Customers 
-group by city 
-having count(customerid) = 2
-union
-select city 
-from Customers 
-group by city
-having count(customerid) > 2;
---5b.
-select city 
-from Customers 
-where city in (select city
-from Customers 
-group by city
-having count(customerid) >= 2);
+--5A
+SELECT CITY 
+FROM CUSTOMERS 
+GROUP BY CITY 
+HAVING COUNT(CUSTOMERID) = 2
+UNION
+SELECT CITY 
+FROM CUSTOMERS 
+GROUP BY CITY
+HAVING COUNT(CUSTOMERID) > 2;
+--5B.
+SELECT CITY 
+FROM CUSTOMERS 
+WHERE CITY IN (SELECT CITY
+FROM CUSTOMERS 
+GROUP BY CITY
+HAVING COUNT(CUSTOMERID) >= 2);
 
 --6
-select c..city , count(distinct p.ProductID) [total products]
-from Products p inner join dbo.[Order Details] od
-on od.ProductID = p.ProductID inner join Orders a 
-on a.OrderID = od.OrderID inner join Customers c 
-on c.CustomerID = a.CustomerID 
-group by c.City 
-having count(distinct p.productid) >= 2
+SELECT C..CITY , COUNT(DISTINCT P.PRODUCTID) [TOTAL PRODUCTS]
+FROM PRODUCTS P INNER JOIN [ORDER DETAILS] OD
+ON OD.PRODUCTID = P.PRODUCTID INNER JOIN ORDERS A 
+ON A.ORDERID = OD.ORDERID INNER JOIN CUSTOMERS C 
+ON C.CUSTOMERID = A.CUSTOMERID 
+GROUP BY C.CITY 
+HAVING COUNT(DISTINCT P.PRODUCTID) >= 2
 
 --7
-select distinct c.CompanyName,c.CustomerID ,c.City ,o.ShipCity
-from Orders o
-inner join Customers c  on c.CustomerID = o.CustomerID 
-where c.City <> o.ShipCity
+SELECT DISTINCT C.COMPANYNAME,C.CUSTOMERID ,C.CITY ,O.SHIPCITY
+FROM ORDERS O
+INNER JOIN CUSTOMERS C  ON C.CUSTOMERID = O.CUSTOMERID 
+WHERE C.CITY <> O.SHIPCITY
 
 --8
-select top 5  p.productname, avg(p.UnitPrice) [Average price], c.city 
-from Products p inner join [Order Details] od on od.ProductID = p.ProductID 
-inner join Orders o on o.OrderID = od.OrderID
-inner join Customers c  on c.CustomerID = o.CustomerID 
-group by p.ProductName, c.city 
-order by sum(od.quantity) desc 
+SELECT TOP 5  P.PRODUCTNAME, AVG(P.UNITPRICE) [AVERAGE PRICE], C.CITY 
+FROM PRODUCTS P INNER JOIN [ORDER DETAILS] OD ON OD.PRODUCTID = P.PRODUCTID 
+INNER JOIN ORDERS O ON O.ORDERID = OD.ORDERID
+INNER JOIN CUSTOMERS C  ON C.CUSTOMERID = O.CUSTOMERID 
+GROUP BY P.PRODUCTNAME, C.CITY 
+ORDER BY SUM(OD.QUANTITY) DESC 
 
---9a
-select city from customers where city  in (select city  from customers a left join orders b on a.customerid =
-b.customerid  and b.employeeid in (select employeeid from orders) group by a.city having count(orderid) = 0) 
---9b
+--9A
+SELECT CITY
+FROM CUSTOMERS
+WHERE CITY  IN (SELECT CITY  
+FROM CUSTOMERS c 
+LEFT JOIN ORDERS o ON c.CUSTOMERID =o.CUSTOMERID 
+AND
+o.EMPLOYEEID IN (SELECT EMPLOYEEID 
+FROM ORDERS)
+GROUP BY c.CITY 
+HAVING COUNT(ORDERID) = 0) 
+--9B
 
-select a.city , count(orderid) from customers a left join orders b on a.customerid  = b.customerid 
-left join employees c on c.EmployeeID = b.EmployeeID  group by a.city having count(orderid) =0 
+SELECT c.CITY , COUNT(ORDERID) 
+FROM CUSTOMERS c 
+LEFT JOIN ORDERS o ON c.CUSTOMERID  = o.CUSTOMERID 
+LEFT JOIN EMPLOYEES e ON e.EMPLOYEEID = o.EMPLOYEEID 
+GROUP BY c.CITY 
+HAVING COUNT(ORDERID) =0 
 
 --10
-select distinct  b.city from orders a inner join customers b on a.CustomerID = b.CustomerID where
-b.city in (select top 1 d.city  from 
-dbo.Products b inner join dbo.[Order Details] a
-on a.ProductID = b.ProductID inner join dbo.Orders c on c.OrderID = a.OrderID
-inner join dbo.Customers d  on d.CustomerID = c.CustomerID group by d.City order by count(c.orderid) desc)
-and 
-b.city in (select top 1 d.city  from 
-dbo.Products b inner join dbo.[Order Details] a
-on a.ProductID = b.ProductID inner join dbo.Orders c on c.OrderID = a.OrderID
-inner join dbo.Customers d  on d.CustomerID = c.CustomerID group by d.City order by count(a.ProductID) desc)
+SELECT DISTINCT  c1.CITY
+FROM ORDERS o INNER JOIN CUSTOMERS c1 ON o.CUSTOMERID = c1.CUSTOMERID
+WHERE c1.CITY IN (SELECT TOP 1 c2.CITY 
+FROM PRODUCTS p 
+INNER JOIN [ORDER DETAILS] od ON od.PRODUCTID = p.PRODUCTID 
+INNER JOIN ORDERS o ON o.ORDERID = od.ORDERID
+INNER JOIN CUSTOMERS c2  ON c2.CUSTOMERID = o.CUSTOMERID 
+GROUP BY c2.CITY 
+ORDER BY COUNT(o.ORDERID) DESC)
+AND 
+c1.CITY IN (SELECT TOP 1 D.CITY  FROM 
+PRODUCTS B INNER JOIN [ORDER DETAILS] od ON od.PRODUCTID = B.PRODUCTID 
+INNER JOIN DBO.ORDERS C ON C.ORDERID = od.ORDERID
+INNER JOIN DBO.CUSTOMERS D  ON D.CUSTOMERID = C.CUSTOMERID 
+GROUP BY D.CITY 
+ORDER BY COUNT(od.PRODUCTID) DESC)
 
---11 Use the group by to find duplicates , then use the delete statements 
+--11 USE THE GROUP BY TO FIND DUPLICATES , THEN USE THE DELETE STATEMENTS 
